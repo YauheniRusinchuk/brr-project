bd5be1142b39222c5d724a07b2c5f4cc58ac26f7
Yauheni Rusinchuk <yauheni_rusinchuk@senla.eu>
2020-03-03 16:19:48 +0300

    admin panel add new components

--- src/components/admin-panel/admin-panel.js
+++ src/components/admin-panel/admin-panel.js
@@ -1,29 +1,21 @@
 import React from 'react'
 import './admin-panel.css'
 
+import {withRouter} from 'react-router-dom'
+
 
 function AdminPanel() {
   return (
     <div className="admin-panel-container">
-      <div className='admin-panel-list-users-in-app'>
-       <div className='admin-panel-list-wrapper'>
-         
-          <p>Lorem  dadkj chkjfh djhwkjdhkjwdw</p>
-          <p>Lorem  dadkj chkjfh djhwkjdhkjwdw</p>
-          <p>Lorem  dadkj chkjfh djhwkjdhkjwdw</p>
-          <p>Lorem  dadkj chkjfh djhwkjdhkjwdw</p>
-          <p>Lorem  dadkj chkjfh djhwkjdhkjwdw</p>
-          <p>Lorem  dadkj chkjfh djhwkjdhkjwdw</p>
-          <p>Lorem  dadkj chkjfh djhwkjdhkjwdw</p>
-          <p>Lorem  dadkj chkjfh djhwkjdhkjwdw</p>
-          <p>Lorem  dadkj chkjfh djhwkjdhkjwdw</p>
-          <p>Lorem  dadkj chkjfh djhwkjdhkjwdw</p>
-          <p>Lorem  dadkj chkjfh djhwkjdhkjwdw</p>
-          <p>Lorem  dadkj chkjfh djhwkjdhkjwdw</p>
-          <p>Lorem  dadkj chkjfh djhwkjdhkjwdw</p>
-          <p>Lorem  dadkj chkjfh djhwkjdhkjwdw</p>
-
-       </div>
+      <div className='admin-panel-wrapper'>
+        <div className='admin-wrapper-item'>
+            <h4>All people in chat</h4>
+            <p>233</p>
+        </div>
+        <div className='admin-wrapper-item'>
+          <h4>All messages</h4>
+          <p>2333</p>
+        </div>
       </div>
     </div>
   )
